90a88c6c42bb395865ef9468311cf8fae57022e0
Marisachyan <49138975+Marisachyan@users.noreply.github.com>
2024-07-30 16:29:09 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+let myHeading = document.querySelector("#test");
+myHeading.textContent = "Hello world!!";
+
+function testSum(num1,num2){
+    let s = num1 + num2;
+    return s;
+}
+
+document.querySelector("html").addEventListener("click", ()=>{
+    alert(11);
+});
